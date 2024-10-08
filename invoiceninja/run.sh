f9be0226b7363c0404d1e2d18f61c96405bebd6f
DerPfandadler <DerPfandadler@users.noreply.github.com>
2024-10-08 16:08:27 +0200

    desperation sets in

--- invoiceninja/run.sh
+++ invoiceninja/run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-# Fetch config values using bashio
+# Custom logic before starting the app
 APP_URL=$(bashio::config 'app_url')
 DB_HOST=$(bashio::config 'db_host')
 DB_USERNAME=$(bashio::config 'db_username')
@@ -8,11 +8,10 @@ DB_PASSWORD=$(bashio::config 'db_password')
 DB_DATABASE=$(bashio::config 'db_database')
 APP_KEY=$(bashio::config 'app_key')
 
-# Export them as environment variables
 export APP_URL DB_HOST DB_USERNAME DB_PASSWORD DB_DATABASE APP_KEY
 
-# Run docker-php-entrypoint without replacing the shell
-/usr/local/bin/docker-php-entrypoint "$@"
+# Call docker-php-entrypoint to handle PHP startup
+exec /usr/local/bin/docker-php-entrypoint "$@"
 
-# Start supervisord after docker-php-entrypoint finishes
+# Optionally, start supervisord after PHP has initialized
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
