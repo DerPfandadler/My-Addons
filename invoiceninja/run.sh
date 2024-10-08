5ccc393be98bf68b57df2ff84f75030404a4061f
DerPfandadler <DerPfandadler@users.noreply.github.com>
2024-10-08 16:16:50 +0200

    add logging

--- invoiceninja/run.sh
+++ invoiceninja/run.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
+
+# Log that the script has started
+bashio::log.info "Starting run.sh script..."
 
 # Fetch config values from Home Assistant
 APP_URL=$(bashio::config 'app_url')
@@ -11,13 +14,13 @@ APP_KEY=$(bashio::config 'app_key')
 # Export them as environment variables
 export APP_URL DB_HOST DB_USERNAME DB_PASSWORD DB_DATABASE APP_KEY
 
-# Log the variables for debugging
-echo "APP_URL=${APP_URL}"
-echo "DB_HOST=${DB_HOST}"
-echo "DB_USERNAME=${DB_USERNAME}"
-echo "DB_PASSWORD=${DB_PASSWORD}"
-echo "DB_DATABASE=${DB_DATABASE}"
-echo "APP_KEY=${APP_KEY}"
+# Log the fetched variables
+bashio::log.info "APP_URL: ${APP_URL}"
+bashio::log.info "DB_HOST: ${DB_HOST}"
+bashio::log.info "DB_USERNAME: ${DB_USERNAME}"
+bashio::log.info "DB_DATABASE: ${DB_DATABASE}"
+bashio::log.info "APP_KEY: ${APP_KEY}"
+
 
 # Call docker-php-entrypoint to handle PHP startup
 exec /usr/local/bin/docker-php-entrypoint "$@"
